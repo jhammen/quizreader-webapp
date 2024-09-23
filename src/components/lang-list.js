27db2019c999bef578c24dae9e0e1b047571dbd5
jhammen <jhammen@j2page.com>
2024-09-23 14:23:29 -0500

    intro text and work in progress warning

--- src/components/lang-list.js
+++ src/components/lang-list.js
@@ -25,12 +25,36 @@ class LangList extends LitElement {
     return html`
       <style>
         a {
-          color: #5555ff;
+          color: #ffc400;
           cursor: pointer;
+        }
+        #subhead {
+          font-weight: bold;
+        }
+        .note {
+          color: orange;
+          border: 2px solid #888888;
+          border-radius: 15px;
+          padding: 10px;
         }
       </style>
       <div>
-        <h2>Available Languages</h2>
+        <h2>Welcome to Quizreader!</h2>
+        <p id="subhead">
+          Quizreader is a free website for foreign language learning.
+        </p>
+        <p>
+          As you read through classic texts the site will quiz you on the new
+          words you encounter. By reading and taking the quizzes you can build a
+          personal vocabulary list of the words you know.
+        </p>
+        <p class="note">
+          ! Quizreader is currently a work in progress. You are more than
+          welcome to test it out but please note the texts are not yet complete
+          and you may lose your progress as we continue to develop the site.
+          Thank you for your patience and understanding.
+        </p>
+        <p>To get started please choose the language you want to learn:</p>
         <ul>
           ${Object.keys(this.languages).map(
             (i) =>
