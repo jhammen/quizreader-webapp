87d4be9c10ad27f4063ed4f642f67364dfd1e5f3
jhammen <jhammen@j2page.com>
2024-11-11 15:26:55 -0500

    edit down intro text, use english names for languages

--- src/components/lang-list.js
+++ src/components/lang-list.js
@@ -32,7 +32,7 @@ class LangList extends LitElement {
           font-weight: bold;
         }
         .note {
-          color: orange;
+          color:  #9c2b2e;
           border: 2px solid #888888;
           border-radius: 15px;
           padding: 10px;
@@ -44,17 +44,11 @@ class LangList extends LitElement {
           Quizreader is a free website for foreign language learning.
         </p>
         <p>
-          As you read through classic texts the site will quiz you on the new
-          words you encounter. By reading and taking the quizzes you can build a
-          personal vocabulary list of the words you know.
+          As you read the site will quiz you on new words. 
+          By reading and taking the quizzes you can build a
+          personal vocabulary list.
         </p>
-        <p class="note">
-          ! Quizreader is currently a work in progress. You are more than
-          welcome to test it out but please note the texts are not yet complete
-          and you may lose your progress as we continue to develop the site.
-          Thank you for your patience and understanding.
-        </p>
-        <p>To get started please choose the language you want to learn:</p>
+        <p>Please choose the language you want to learn:</p>
         <ul>
           ${Object.keys(this.languages).map(
             (i) =>
@@ -66,7 +60,12 @@ class LangList extends LitElement {
                 ></app-link>
               </li>`
           )}
+          <li><img height="20" width="30" src="fr/flag.png" /> French (coming soon)</li>
         </ul>
+        <p class="note">
+          ⚠️ Quizreader is a work in progress. You are welcome to test it but the texts are not yet complete
+          and you may lose your progress as we continue to develop the site. Thank you.
+        </p>
       </div>
     `;
   }
